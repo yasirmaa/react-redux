d4ac826a26d807d720228d42cbc0911f7781024e
yasirmaa <yasirmaarif26@gmail.com>
2024-09-15 10:40:51 +0700

    form with zod & react-hook-form

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,65 +1,105 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
 import { Checkbox } from '@/components/ui/checkbox';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+type LoginForm = {
+  email: string;
+  password: string;
+};
+
+const loginFromSchema = z.object({
+  email: z.string().email('Invalid email format'),
+  password: z.string().min(8, 'Password must be at least 8 characters long'),
+});
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const form = useForm({
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+    resolver: zodResolver(loginFromSchema),
+    reValidateMode: 'onChange',
+  });
+
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleLogin = () => {
-    console.log({ email, password });
+  const handleLogin = (values: LoginForm) => {
+    console.log(values.email, values.password);
   };
 
   return (
     <main className="px-4 container py-8 flex flex-col mx-auto justify-center items-center max-w-screen-md h-[80vh]">
-      <Card className="w-full max-w-[540px]">
-        <CardHeader>
-          <CardTitle>Welcome back!</CardTitle>
-        </CardHeader>
-        <CardContent className="flex flex-col gap-2">
-          <div className="">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              type="email"
-              id="email"
-              name="email"
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-            />
-          </div>
-          <div className="">
-            <Label htmlFor="password">Password</Label>
-            <Input
-              type={showPassword ? 'text' : 'password'}
-              id="password"
-              name="password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-            />
-          </div>
-          <div className="flex items-center space-x-2">
-            <Checkbox
-              id="show-password"
-              onCheckedChange={(checked) => setShowPassword(Boolean(checked))}
-            />
-            <Label htmlFor="show-password">Show Password</Label>
-          </div>
-        </CardContent>
-        <CardFooter>
-          <div className="flex flex-col space-y-4 w-full">
-            <Button onClick={handleLogin}>Sign in</Button>
-            <Button variant={'link'} className="w-full">
-              Sign Up instead
-            </Button>
-          </div>
-        </CardFooter>
-      </Card>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(handleLogin)} className="w-full max-w-[540px]">
+          <Card>
+            <CardHeader>
+              <CardTitle>Welcome back!</CardTitle>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-2">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="password"
+                        {...field}
+                        type={showPassword ? 'text' : 'password'}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex items-center space-x-2">
+                <Checkbox
+                  id="show-password"
+                  onCheckedChange={(checked) => setShowPassword(Boolean(checked))}
+                />
+                <Label htmlFor="show-password">Show Password</Label>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <div className="flex flex-col space-y-4 w-full">
+                <Button type="submit">Sign in</Button>
+                <Button variant={'link'} className="w-full">
+                  Sign Up instead
+                </Button>
+              </div>
+            </CardFooter>
+          </Card>
+        </form>
+      </Form>
     </main>
   );
 };
